b199358413fd34d034d991297212cbf0d508e409
Stephen Anderson and Jordan Peterson <dev+bendycode+dysolution@bendyworks.com>
2012-05-12 14:00:45 -0500

    dynamic tweet retrieval with user-defined search

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.3'
 gem 'haml-rails'
 gem 'pg'
 gem 'jquery-rails'
+gem 'twitter'
 
 
 # Gems used only for assets and not required
--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,4 +1,6 @@
 class ExamplesController < ApplicationController
   def index
+    @query = params[:q] || 'your welcome'
+    @tweets = Twitter.search("\"#{@query}\"", :result_type=> 'recent')
   end
 end
