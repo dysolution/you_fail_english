a0205cfcbe3d4155a9b7f41315c558da1862ea38
Stephen Anderson and Jordan Peterson <dev+bendycode+dysolution@bendyworks.com>
2012-05-12 09:40:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YouFailEnglish::Application.config.secret_token = 'b7ef7f93ab886f5a2134f2779daa6fec2f2804776699770eb9242bb630fe411d63a13c1d68e5d979a782168d14c2339ff9d9b7ba8d26c24b96bc4287f757eeec'
